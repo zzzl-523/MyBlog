fb74bd4afc5b7842edd35a7fba3b8f127e3c86b0
Jihyun Lee <zzh23@naver.com>
2024-01-04 15:23:25 +0900

    Feature/Home navigation btn

--- my-blog/src/components/PostList.tsx
+++ my-blog/src/components/PostList.tsx
@@ -1,16 +1,31 @@
 import PostBox from "components/PostBox";
+import { useState } from "react";
 
 interface PostListProps {
   hasNavigation?: boolean;
 }
 
 export default function PostList({ hasNavigation = false }: PostListProps) {
+  const [activeTab, setActiveTab] = useState("all");
+
   return (
     <>
       {hasNavigation && (
         <div className="post__navigation">
-          <div className="post__navigation-active">전체</div>
-          <div>나의 글</div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("all")}
+            className={activeTab === "all" ? "post__navigation-active" : ""}
+          >
+            전체
+          </div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("my")}
+            className={activeTab === "my" ? "post__navigation-active" : ""}
+          >
+            나의 글
+          </div>
         </div>
       )}
 
