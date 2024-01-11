4651fca25838eb6e5a89d793d3186cfeb8a8b0ca
Jihyun Lee <zzh23@naver.com>
2024-01-11 15:26:05 +0900

    Feature/isAuthenticated -> Routing

--- my-blog/src/components/Router.tsx
+++ my-blog/src/components/Router.tsx
@@ -7,22 +7,33 @@ import PostEditPage from "pages/posts/edit";
 import PostNewPage from "pages/posts/new";
 import ProfilePage from "pages/profile";
 import SignupPage from "pages/signup";
+import { useState } from "react";
 
 const Router = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostListPage />} />
-        <Route path="/posts/:id" element={<PostDetailPage />} />
-        <Route path="/posts/new" element={<PostNewPage />} />
-        <Route path="/posts/edit/:id" element={<PostEditPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </>
+    <Routes>
+      {isAuthenticated ? (
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<PostListPage />} />
+          <Route path="/posts/:id" element={<PostDetailPage />} />
+          <Route path="/posts/new" element={<PostNewPage />} />
+          <Route path="/posts/edit/:id" element={<PostEditPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<Navigate replace to="/login" />} />
+        </>
+      )}
+    </Routes>
   );
 };
 
