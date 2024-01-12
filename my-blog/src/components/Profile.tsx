ba74573a462b0c932af8c423109ae213f3748dea
Jihyun Lee <zzh23@naver.com>
2024-01-12 19:17:16 +0900

    Feature/Logout

--- my-blog/src/components/Profile.tsx
+++ my-blog/src/components/Profile.tsx
@@ -1,20 +1,41 @@
 import { Link } from "react-router-dom";
 import PostList from "components/PostList";
+import { getAuth, signOut } from "@firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
+
+const onSignOut = async () => {
+  try {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃 되었습니다");
+  } catch (error: any) {
+    toast.error(error);
+    console.log(error.code);
+  }
+};
 
 export default function Profile() {
+  const auth = getAuth(app);
   return (
     <>
       <div className="profile__box">
         <div className="flex__box-lg">
           <div className="profile__image" />
           <div>
-            <div className="profile__email">test@test.com</div>
-            <div className="profile__name">이름</div>
+            <div className="profile__email">{auth?.currentUser?.email}</div>
+            <div className="profile__name">
+              {auth?.currentUser?.displayName || "사용자"}
+            </div>
           </div>
         </div>
-        <Link to="/" className="profile__logout">
+        <div
+          role="presentation"
+          className="profile__logout"
+          onClick={onSignOut}
+        >
           로그아웃
-        </Link>
+        </div>
       </div>
       <PostList />
     </>
