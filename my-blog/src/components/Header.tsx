934a441ba9995568557d5b202b24fcace8dfa67e
Jihyun Lee <zzh23@naver.com>
2024-01-16 15:38:47 +0900

    Feature/Dark mode 컴포넌트 & localStorage 사용

--- my-blog/src/components/Header.tsx
+++ my-blog/src/components/Header.tsx
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
+import { BsMoonFill, BsSun } from "react-icons/bs";
+import { useContext } from "react";
+import { ThemeContext } from "context/ThemeContext";
 
 export default function Header() {
+  const theme = useContext(ThemeContext);
+
   return (
     <header className="header">
-      <Link to="/" className="header__logo">
-        MyBlog
-      </Link>
+      <div className="header__block">
+        <Link to="/" className="header__logo">
+          MyBlog
+        </Link>
+        <div className="theme-icon">
+          {theme.theme === "light" ? (
+            <BsSun className="theme-icon" onClick={theme.toggleMode} />
+          ) : (
+            <BsMoonFill className="theme-icon" onClick={theme.toggleMode} />
+          )}
+        </div>
+      </div>
 
       <div>
         <Link to="/posts/new">글쓰기</Link>
