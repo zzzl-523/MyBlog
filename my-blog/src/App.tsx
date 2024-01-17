adca12a74361edc8a0bffe6314fcc22fb2bb16f1
Jihyun Lee <zzh23@naver.com>
2024-01-17 16:13:30 +0900

    Featrue/Dark Mode CSS

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,12 +1,14 @@
 import Router from "components/Router";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader";
+import { ThemeContext } from "context/ThemeContext";
 
 function App() {
+  const theme = useContext(ThemeContext);
   // loader(로딩) 추가
   const [init, setInit] = useState<boolean>(false);
   const auth = getAuth(app);
@@ -27,13 +29,13 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={theme.theme === "light" ? "light" : "dark"}>
       <ToastContainer />
       {
         // 로딩 전에는 init, 로딩 후에는 화면 보여주기
         init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />
       }
-    </>
+    </div>
   );
 }
 
